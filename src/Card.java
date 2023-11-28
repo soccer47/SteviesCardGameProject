2f08ddf6c57667e32b72d8d6e2c30c8a8f1de4a5
steviekh <stevie.halprin@menloschool.org>
2023-11-27 19:27:35 -0800

    I completed the Card Deck and Card classes.

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,41 @@
 public class Card {
+    private String suit;
+    private String rank;
+    private int value;
+
+    public Card(String suit, String rank, int value)
+    {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+
+    public String getSuit(){
+        return this.suit;
+    }
+
+    public String getRank(){
+        return this.rank;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public void setSuit(String newSuit){
+        this.suit = newSuit;
+    }
+
+    public void setRank(String newRank){
+        this.suit = newRank;
+    }
+
+    public void setValue(int newValue){
+        this.value = newValue;
+    }
+
+    public String toString(){
+        return this.rank + " of " + this.suit;
+    }
+
 }
---
+++ src/CardDeck.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+public class CardDeck{
+    private ArrayList<Card> Deck;
+    private int cardsLeft;
+
+    public CardDeck(String[] ranks, String[] suits, int[] values){
+        Deck = new ArrayList<Card>();
+        for (int i = 0; i < ranks.length; i++){
+            for (String suit : suits){
+               Deck.add(new Card(suit, ranks[i], values[i]));
+            }
+        }
+        cardsLeft = Deck.size();
+        shuffle();
+    }
+
+    public boolean isEmpty(){
+        return cardsLeft == 0;
+    }
+
+    public int getCardsLeft(){
+        return cardsLeft;
+    }
+
+    public Card deal(){
+        if (cardsLeft == 0) {
+            return null;
+        }
+        cardsLeft--;
+        return Deck.get(cardsLeft);
+    }
+
+    public void shuffle(){
+        ArrayList<Card> newDeck = new ArrayList<Card>();
+        while (!Deck.isEmpty()){
+            newDeck.add(Deck.remove((int)(Math.random() * Deck.size())));
+        }
+        for (Card i : newDeck){
+            Deck.add(i);
+        }
+        cardsLeft = Deck.size();
+    }
+
+}
